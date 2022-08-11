645e204e4445e49c9bdaff164b430bda30d0b33d
Phillipe Augusto de Araújo Mendonça <phillipe.a.a.mendonca@gmail.com>
2022-08-11 16:41:00 -0300

    Added a utility which pings Google

---
+++ Scripts/InternetHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class InternetHelper
+{
+    private const string GoogleSite = "https://google.com";
+
+    public static IEnumerator CheckInternetConnection(Action<bool> action)
+    {
+        var uwr = new UnityWebRequest(GoogleSite);
+
+        yield return uwr.SendWebRequest();
+
+        var wasSuccess = uwr.error == null;
+        action?.Invoke(wasSuccess);
+    }
+}
