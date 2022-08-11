0e306cbc5db546abe97d24f20b87a4bfec74d118
Phillipe Augusto de Araújo Mendonça <phillipe.a.a.mendonca@gmail.com>
2022-08-11 09:58:27 -0300

    Added an UIInteractionDetector

---
+++ Scripts/UIInteractionDetector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIInteractionDetector : MonoBehaviour
+{
+    [SerializeField] private EventSystem _eventSystem;
+
+    private PointerEventData _pointerEventData;
+    private List<RaycastResult> _results;
+
+    private void Start() => InitVariables();
+
+    private void InitVariables()
+    {
+        _results = new List<RaycastResult>();
+        _pointerEventData = new PointerEventData(_eventSystem);
+    }
+
+    public bool IsPointerOverUI()
+    {
+        _results.Clear();
+        _pointerEventData.position = Input.mousePosition;
+        _eventSystem.RaycastAll(_pointerEventData, _results);
+
+        return _results.Count > 0;
+    }
+}
