b7f288f5f7eaa30e0dc39b4984b154506321dbde
Phillipe Augusto de Araújo Mendonça <phillipe.a.a.mendonca@gmail.com>
2022-08-11 09:54:14 -0300

    Added a SingleInScene reference script

---
+++ Scripts/SingleInScene.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SingleInScene<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (Instance == null)
+            Instance = this as T;
+        else
+            Destroy(gameObject);
+    }
+}
