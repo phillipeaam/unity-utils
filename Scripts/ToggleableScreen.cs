1a3b8409919ff50742d88a8585b68b620f52114f
Phillipe Augusto de Araújo Mendonça <phillipe.a.a.mendonca@gmail.com>
2022-08-16 11:15:03 -0300

    Added a class that toggles a canvas group On/OFF

---
+++ Scripts/ToggleableScreen.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ToggleableScreen : MonoBehaviour
+{
+    [SerializeField] protected CanvasGroup _canvasGroup;
+
+    protected void ToggleCanvasGroup(bool isVisible)
+    {
+        _canvasGroup.alpha = isVisible ? 1 : 0;
+        _canvasGroup.interactable = isVisible;
+        _canvasGroup.blocksRaycasts = isVisible;
+    }
+}
